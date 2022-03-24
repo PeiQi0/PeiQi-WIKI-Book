ac3887a2ef3285b1e46b85762996fa0b9087e8ab
System Administrator <root@192.168.0.144>
2022-03-24 11:29:55 +0800

    安全招聘:瑞森网安

--- docs/cooperation/sidebar_contents.js
+++ docs/cooperation/sidebar_contents.js
@@ -7,7 +7,7 @@ module.exports = [
 			"/cooperation/recruit/HW2022-初中级蓝队招募",
 			"/cooperation/recruit/北京微步在线科技有限公司",
 			"/cooperation/recruit/安天科技集团股份有限公司",
-			"/cooperation/recruit/瑞森网安(福建)信息科技有限公司",
+			"/cooperation/recruit/瑞森网安信息科技有限公司",
 		]
 	},
 	{
