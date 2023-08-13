d0bf346dc63d5dc66d8766b6c8094703ed0dcbbf
WgpSec Continuous Integration <ci@wgpsec.org>
2023-08-13 08:57:12 +0000

    deploy: c6ceb02e1756c42778a6035b5745b67cd4b7f41c

---
+++ assets/js/635.213eefb3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[635],{2852:function(t,s,n){"use strict";n.r(s);var e=n(64),o=Object(e.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("p",[t("img",{attrs:{src:"/img/home.svg",alt:""}})])])}),[],!1,null,null,null);s.default=o.exports}}]);
